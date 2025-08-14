00f46bb7c4a45d4c48d5736aaf4777c3aded94c0
Clarityadvisory01 <clarity.advisory01@gmail.com>
2025-08-15 00:52:16 +0530

    Update AboutUsContent.tsx
    testing

--- src/components/public/web/features/about-us/AboutUsContent.tsx
+++ src/components/public/web/features/about-us/AboutUsContent.tsx
@@ -6,7 +6,7 @@ export default function AboutUsContent() {
       <div className="text-center text-lg text-gray-800">
         <h1 className="text-3xl font-bold mb-4">About Us</h1>
         <p>
-          Welcome to our company. We are committed to building scalable and impactful web solutions.
+          Welcome to our company test1. We are committed to building scalable and impactful web solutions.
         </p>
       </div>
     </Container>
