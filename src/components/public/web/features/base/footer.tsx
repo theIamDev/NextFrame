5c78c4dd7e23d16190cd99ae1ecc95190bf99bf0
iampayal2004 <singh0401.payal@gmail.com>
2025-08-17 18:35:29 +0530

    This project is a modern About us section built using Tailwind CSS. It features a split layout with an image on one side and content on other side

--- src/components/public/web/features/base/footer.tsx
+++ src/components/public/web/features/base/footer.tsx
@@ -3,20 +3,7 @@
 import FooterUI from "../../ui/footerUi";
 
 const Footer = () => {
-  const logo = {
-    src: "/next.svg",
-    alt: "CLM Footer Logo",
-    href: "/",
-  };
-
-  const links = [
-    { label: "Privacy Policy", href: "/privacy" },
-    { label: "Terms of Service", href: "/terms" },
-    { label: "Contact", href: "/contact" },
-  ];
-
-  const copyright = `© ${new Date().getFullYear()} CLM Portal. All rights reserved.`;
-
+  
   return <FooterUI  />;
 };
 
